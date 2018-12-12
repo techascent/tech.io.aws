8b4c51a899819ce01e1b67c28f5eb2509e9717ba
Chris Nuernberger <chris@thinktopic.com>
2018-12-12 16:34:12 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io.aws "1.1"
+(defproject techascent/tech.io.aws "1.2-SNAPSHOT"
   :description "aws bindings for io subsystem"
   :url "http://github.com/tech-ascent/tech.io.aws"
   :license {:name "Eclipse Public License"
