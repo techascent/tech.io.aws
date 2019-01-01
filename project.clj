73a71d9c5ea513afbc3727495a66c970c0a5d4c1
Chris Nuernberger <chris@thinktopic.com>
2019-01-01 14:55:08 -0700

    1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io.aws "1.5-SNAPSHOT"
+(defproject techascent/tech.io.aws "1.5"
   :description "aws bindings for io subsystem"
   :url "http://github.com/tech-ascent/tech.io.aws"
   :license {:name "Eclipse Public License"
