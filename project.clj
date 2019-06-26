4986db23c01f9640e769375af640f8418b7dfc4d
Chris Nuernberger <chris@techascent.com>
2019-06-26 08:03:14 -0600

    upgraded to tech.io 3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io.aws "1.7-SNAPSHOT"
+(defproject techascent/tech.io.aws "3.0-SNAPSHOT"
   :description "aws bindings for io subsystem"
   :url "http://github.com/tech-ascent/tech.io.aws"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
               [lein-eftest "0.5.3"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :repositories {"releases"  {:url "s3p://techascent.jars/releases/"
-                              :no-auth true
-                              :sign-releases false}}
-  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
+  :profiles {:dev {:dependencies [
+                                  ;;[techascent/vault-clj "0.2.21"]
+                                  [amperity/vault-clj "0.7.0"]
+                                  ]}})
--- test/tech/io/s3_test.clj
+++ test/tech/io/s3_test.clj
@@ -1,8 +1,10 @@
 (ns tech.io.s3-test
-  (:require [tech.io.s3]
+  (:require [tech.io.s3 :as s3]
             [tech.io :as io]
+            [tech.io.auth :as io-auth]
             [tech.io.temp-file :as temp-file]
             [tech.io.providers :as providers]
+            [tech.config.core :as config]
             [clojure.test :refer :all])
   (:import [java.io File]))
 
@@ -11,19 +13,24 @@
 
 ;;These tests only work if you have vault access credentials
 
-
 (deftest s3-ls
-  (temp-file/with-temp-dir
-    temp-dir
-    (io/with-provider (->> [(providers/vault-auth-provider nil {})
-                            (providers/caching-provider temp-dir {})]
-                           providers/provider-seq->wrapped-providers)
-      (let [result (io/ls "s3://techascent.test/")
-            first-file (->> result
-                            (remove :directory?)
-                            first
-                            :url
-                            io/get-object)]
-        ;;Caching should always return a file.
-        (is (instance? File first-file))
-        (is (> (count result) 0))))))
+  (with-bindings {#'s3/*default-s3-provider*
+                        (io-auth/authenticated-provider
+                         (s3/s3-provider
+                          {:tech.aws/endpoint (config/get-config :tech-aws-endpoint)})
+                         (io-auth/vault-aws-auth-provider (tech.config.core/get-config
+                                                           :tech-vault-aws-path)
+                                                          {:re-request-time-ms 4000}))}
+    (temp-file/with-temp-dir
+      temp-dir
+      (io/with-provider (->> [(providers/caching-provider temp-dir {})]
+                             providers/provider-seq->wrapped-providers)
+        (let [result (io/ls "s3://techascent.test/")
+              first-file (->> result
+                              (remove :directory?)
+                              first
+                              :url
+                              io/get-object)]
+          ;;Caching should always return a file.
+          (is (instance? File first-file))
+          (is (> (count result) 0)))))))
