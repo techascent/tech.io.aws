c637f7a0a9a2ec1f89d16afe449746f3ea4072a4
Harold <hhausman@gmail.com>
2020-07-08 09:59:04 -0600

    [s3] First cut at creds from config.

--- project.clj
+++ project.clj
@@ -3,11 +3,8 @@
   :url "http://github.com/tech-ascent/tech.io.aws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :plugins [[lein-tools-deps "0.4.1"]
-              [lein-eftest "0.5.3"]]
+  :plugins [[lein-tools-deps "0.4.1"]
+            [lein-eftest "0.5.3"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:dependencies [
-                                  ;;[techascent/vault-clj "0.2.21"]
-                                  [amperity/vault-clj "0.7.0"]
-                                  ]}})
+  :profiles {:dev {:dependencies [[amperity/vault-clj "0.7.0"]]}})
--- test/tech/io/s3_test.clj
+++ test/tech/io/s3_test.clj
@@ -1,10 +1,9 @@
 (ns tech.io.s3-test
-  (:require [tech.io.s3 :as s3]
+  (:require [clojure.test :refer [deftest is]]
+            [tech.io.s3 :as s3]
             [tech.io :as io]
             [tech.io.auth :as io-auth]
-            [tech.io.temp-file :as temp-file]
-            [tech.config.core :as config]
-            [clojure.test :refer :all])
+            [tech.config.core :as config])
   (:import [java.io File]))
 
 
@@ -14,18 +13,18 @@
 
 (deftest s3-ls
   (with-bindings {#'s3/*default-s3-provider*
-                        (io-auth/authenticated-provider
-                         (s3/s3-provider
-                          {:tech.aws/endpoint (config/get-config :tech-aws-endpoint)})
-                         (io-auth/vault-aws-auth-provider (tech.config.core/get-config
-                                                           :tech-vault-aws-path)
-                                                          {:re-request-time-ms 4000}))}
+                  (io-auth/authenticated-provider
+                   (s3/s3-provider
+                    {:tech.aws/endpoint (config/get-config :tech-aws-endpoint)})
+                   (io-auth/vault-aws-auth-provider (tech.config.core/get-config
+                                                     :tech-vault-aws-path)
+                                                    {:re-request-time-ms 4000}))}
     (let [result (io/ls "s3://techascent.test/")
           first-file (->> result
                           (remove :directory?)
                           first
-                              :url
-                              io/get-object)]
+                          :url
+                          io/get-object)]
       ;;Caching should always return a file.
       (is (not (nil? first-file)))
       (is (> (count result) 0)))))
