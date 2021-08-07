db2034691e197ec94c4ffec947599b3a068b91bb
Chris Nuernberger <chris@techascent.com>
2021-08-07 13:33:15 -0600

    4.08-3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io.aws "4.08-3-SNAPSHOT"
+(defproject techascent/tech.io.aws "4.08-3"
   :description "aws bindings for io subsystem"
   :url "http://github.com/tech-ascent/tech.io.aws"
   :license {:name "Eclipse Public License"
